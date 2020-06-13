7c77ba2c5c171a2c6fbfedbaba8f18781e365d50
Hurley Swan <antontit89@gmail.com>
2020-06-13 22:04:49 +0700

    Created Speciality, Position models

--- scraping/admin.py
+++ scraping/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import City
+from .models import City, Speciality, Position
 
 admin.site.register(City)
+admin.site.register(Speciality)
+admin.site.register(Position)
--- scraping/models.py
+++ scraping/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from .utils import from_cyrillic_to_eng
 
 class City(models.Model):
-    city_name = models.CharField(max_length=50, verbose_name='Название города')
-    slug = models.CharField(max_length=50, blank=True)
+    city_name = models.CharField(max_length=50, verbose_name='Название города', unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
 
     class Meta:
         verbose_name = 'Название города'
@@ -10,3 +11,43 @@ class City(models.Model):
     
     def __str__(self):
         return self.city_name   
+    
+    def save(self, *args, **kwargs): 
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(str(self.city_name)) #поскольку city_name - CharField а не str
+        super().save(*args, **kwargs) 
+
+class Speciality(models.Model):
+    speciality_name = models.CharField(max_length=50, verbose_name='Название специальности', unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Название специальности'
+        verbose_name_plural = 'Названия специальностей'
+    
+    def __str__(self):
+        return self.speciality_name   
+    
+    def save(self, *args, **kwargs): 
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(str(self.speciality_name)) #поскольку city_name - CharField а не str
+        super().save(*args, **kwargs) 
+
+class Position(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=250, verbose_name='Название вакансии')
+    company = models.CharField(max_length=250, verbose_name='Имя компании')
+    description = models.TextField(verbose_name='Полное описание вакансии')
+    city = models.ForeignKey('City', on_delete=models.CASCADE, verbose_name='Город вакансии')
+    speciality = models.ForeignKey('Speciality', on_delete=models.CASCADE, verbose_name='Специализация вакансии')
+    added_on = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Название вакансии'
+        verbose_name_plural = 'Доступные вакансии'
+    
+    def __str__(self):
+        return self.title   
+
+
+    
